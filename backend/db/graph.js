406347c9fe7d8d409e4323f574c83d0fa15494bd
dan2014 <dmen326@gmail.com>
2019-08-26 18:04:50 -0400

    changed the db functions to a class that can chain transactions

--- backend/db/graph.js
+++ backend/db/graph.js
@@ -1,97 +1,69 @@
 const db = require('../data/config');
 
-module.exports = {
-    addRoom,
-    getRoom,
-    currentRoom,
-    move,
-    getGraph
+module.exports = trxClass
+
+function trxClass(){
+    return new TRX_Chain
 }
 
-async function move(prevRoomID,movement,currRoomData){
-    const trx = await db.transaction();
-    try{ 
-        const currRoomID = currRoomData['room_id']
-        let room = await trx('graph')
-            .select("room_id")
-            .where({"room_id":currRoomID})
-            .first()
 
-        // if room doesn't exists in db, create entry
-        if(room === undefined){
-             
-            const jsonString = JSON.stringify(currRoomData)
-            room = await trx('graph')
-                .insert({'room_id':currRoomID,'room_data':jsonString})
-                .returning('*')
+class TRX_Chain {
 
-            // Insert available directions
-            room = room[0]
-            const exits = currRoomData.exits.map(exit => {
-                return {"from_room_FK":room['room_id'],"direction":exit}
-            })
-                
-            const test_dir = await trx('directions').insert(exits);
-        } 
-
-        
-        room = room['room_id']
-        let vertex = await trx('directions')
-            .where({'from_room_FK':prevRoomID,"direction":movement,'to_room_FK':room})
-            .returning('*')
-            .first()
-
-        // if vertices don't exists in db, create entry
-        if(vertex === undefined){
-             // Insert Vertices
-            const direc = {'s':'n','n':'s','e':'w','w':'e'}
-            const vertices = [{"from_room_FK":prevRoomID,"direction":movement,'to_room_FK':room} ,{'from_room_FK':room,"direction":direc[movement],'to_room_FK':prevRoomID}]
-            vert_prom = vertices.map(e => {
-                return trx('directions').where('from_room_FK',e['from_room_FK']).andWhere('direction',e['direction']).update('to_room_FK',e['to_room_FK']).returning('*');
-            })
-            
-            return Promise.all(vert_prom)
-            .then( (e) => {
-                trx.commit()
-                return e
-            }
-            )
-            .catch((e) => {
-                console.log(e.message)
-                throw new Error("An error has occured")
-            } )
-        }
-        
-    }catch(e){
-        console.log("An error has occured")
-        trx.rollback()
+    constructor (){
+        this.trxProvider = db.transactionProvider()
+        this.trx = null
     }
-}
 
+    async getRoom(roomID){
+        this.trx = await this.trxProvider()
+        try{
+            
+            const res = await TRX_Chain.getRoom_trx(roomID,this.trx)
+            this.trx.commit()
+            return res
+        }catch(e){
+            this.trx.rollback()
+            throw e
+        }
+    }
 
-function currentRoom() {
-    return db('graph')
-        .orderBy('created_at')
-        .first()
-        .then(entry => {
-            return entry
-        })
-        .catch(err => {
-            return err
-        });
-}
-
-async function addRoom(currRoomData) {
-    const trx = await db.transaction();
-    try{ 
-        const currRoomID = currRoomData["room_id"]
-        let room = await trx('graph')
-            .where({"room_id":currRoomID})
+    static async getRoom_trx(roomID,trx){
+        try{
+            const res = await trx('graph')
+            .where({"room_id":roomID})
             .returning('*')
             .first()
+            return res
+        }catch(e){
+            throw e
+        }
+    }
 
-        if(room === undefined){
-             // room doesn't exists in db, create entry
+
+    async addRoom(roomData){
+        this.trx = await this.trxProvider()
+        try{
+            // Get room
+            const currRoomID = roomData["room_id"]
+            const room = await TRX_Chain.getRoom_trx(currRoomID,this.trx)
+
+            if(room === undefined){
+                // Add room 
+                var addedRoom = await TRX_Chain.addRoom_trx(roomData,this.trx)
+            }
+
+            this.trx.commit()
+            return addedRoom
+        }catch(e){
+            this.trx.rollback()
+            throw e
+        }
+    }
+
+    static async addRoom_trx(currRoomData,trx){
+        try{
+            // room doesn't exists in db, create entry
+            const currRoomID = currRoomData["room_id"]
             const jsonString = JSON.stringify(currRoomData)
             room = await trx('graph')
                 .insert({'room_id':currRoomID,'room_data':jsonString})
@@ -103,63 +75,115 @@ async function addRoom(currRoomData) {
             })
                 
             await trx('directions').insert(exits);
-        } 
-        
-        trx.commit()
-        return room
-    }catch(e){
-        console.log(e.message)
-        console.log("An error has occured in add room")
-        trx.rollback()
+            return room
+        }catch(e){
+            throw e
+        }
+       
     }
-}
 
-function getRoom(roomID,trx=null) {
-    if( trx === null){
-        var test = trx
-    }else{
-        var test = db
-    }
-    return test('graph')
-        .where({"room_id":roomID})
-        .returning('*')
-        .first()
-        .then(entry => {
-            return entry
-        })
-        .catch(err => {
-            return err
-        });
-}
+    async move(prevRoomID,movement,currRoomData){
+        this.trx = await this.trxProvider()
+        try{
+            const currRoomID = currRoomData["room_id"]
+            let room = await TRX_Chain.getRoom_trx(currRoomID,this.trx)
 
-async function getGraph() {
-    const trx = await db.transaction();
-    try{
-        const rooms = await trx('graph').select('room_id')
-        const graph_prom = rooms.map(room => {
-            return trx('directions')
-            .select('to_room_FK','direction')
-            .where('from_room_FK',room['room_id'])
-            })
-
-        return Promise.all(graph_prom)
-            .then( (e) => {
-
-                trx.commit()
-                graph = {}
-                e.forEach( (room,index) => {
-                    graph[rooms[index]['room_id']] = room
-
-                })
-                return graph
+            if(room === undefined){
+                // Add room 
+                room = await TRX_Chain.addRoom_trx(currRoomData,this.trx)
             }
-            )
-            .catch((e) => {
-                console.log(e.message)
-                throw new Error("An error has occured")
-            } )
-    }catch(e){
-        console.log(e)
-        trx.rollback()
+
+            const res = await TRX_Chain.move_trx(prevRoomID,movement,currRoomID,this.trx)
+
+            this.trx.commit()
+            return res
+        }catch(e){
+            this.trx.rollback()
+            throw e
+        }
     }
+
+    static async move_trx(prevRoomID,movement,room,trx){
+        try{
+            // room = room['room_id']
+            let vertex = await trx('directions')
+                .where({'from_room_FK':prevRoomID,"direction":movement,'to_room_FK':room})
+                .returning('*')
+                .first()
+    
+            // if vertices don't exists in db, create entry
+            if(vertex === undefined){
+                 // Insert Vertices
+                const direc = {'s':'n','n':'s','e':'w','w':'e'}
+                const vertices = [{"from_room_FK":prevRoomID,"direction":movement,'to_room_FK':room} ,{'from_room_FK':room,"direction":direc[movement],'to_room_FK':prevRoomID}]
+                const vert_prom = vertices.map(e => {
+                    return trx('directions').where('from_room_FK',e['from_room_FK']).andWhere('direction',e['direction']).update('to_room_FK',e['to_room_FK']).returning('*');
+                })
+                
+                return Promise.all(vert_prom)
+                .then( (e) => {
+                    return e
+                }
+                )
+                .catch((e) => {
+                    console.log(e.message)
+                    throw new Error("An error has occured")
+                } )
+            }
+
+        }catch(e){
+            throw e
+        }
+    }
+
+
+    async getGraph(){
+        this.trx = await this.trxProvider()
+        try{
+            const res = await TRX_Chain.getGraph_trx(this.trx)
+            this.trx.commit()
+            return res
+        }catch(e){
+            this.trx.rollback()
+            throw e
+        }
+    }
+
+    static async getGraph_trx(trx){
+        try{
+            // get all rooms
+            const rooms = await trx('graph').select('room_id')
+            // get all directions
+            const graph_prom = rooms.map(room => {
+                return trx('directions')
+                .select('to_room_FK','direction')
+                .where('from_room_FK',room['room_id'])
+                })
+    
+            return Promise.all(graph_prom)
+                .then( (e) => {
+                    const graph = {}
+                    e.forEach( (room,index) => {
+                        let direction = {}
+                        room.forEach( dir => {
+                            const key = dir["direction"]
+                            direction[key] =  dir['to_room_FK']
+                        })
+                        const id = rooms[index]['room_id']
+                        graph[id] = direction
+    
+                    })
+                    return graph
+                }
+                )
+                .catch((e) => {
+                    console.log(e.message)
+                    throw new Error("An error has occured test")
+                } )
+
+        }catch(e){
+            throw e
+        }
+    }
+
 }
--- backend/traversal/traversal.js
+++ backend/traversal/traversal.js
@@ -8,7 +8,7 @@ const db = require("../db/graph")
 // }
 
 async function traversal(room){
-    const currRoom = await currentRoom()
+    // const currRoom = await currentRoom()
     // if currentRoom is undefined, then the db is empty. The db can be empty from starting for the first time, 
     // or db rollback
     // request from the init endpoint to start db
@@ -16,6 +16,8 @@ async function traversal(room){
     // const newRoom = await addRoom(room)
     // console.log(currRoom,"current room")
     // console.log(newRoom,"new room")
+    const testing = await db().getRoom(0)
+    console.log(testing,"testing trx_chain")
 
     // handle undefined result from resolved promise
     room0 = {
@@ -30,45 +32,18 @@ async function traversal(room){
         "errors": [], 
         "messages": []
     }
-    const firstRoom = await db.addRoom(room0)
-    console.log(firstRoom)
-    const res = await db.move(0,"n",room)
-    console.log(res,"get Room")
-    const graph = await db.getGraph()
+
+    const addedRoom = await db().addRoom(room0)
+    console.log(addedRoom,"add room testing trx_chain")
+
+    const room10 = await db().move(0,"n",room)
+    console.log(room10,"move room testing trx_chain")
+
+    const graph = await db().getGraph()
     console.log(graph)
 }
 
-// async function addRoom(room){
-//     const id = room.room_id
-//     const newRoom = await db.addRoom(id,room)
-//     return newRoom
-// }
 
-async function getRoom(id){
-    const room = await db.getRoom(id)
-    return room
-}
-
-async function move(previousRoom,currentRoom){
-    const id = room.room_id
-    const newRoom = await db.addRoom(id,room)
-    return newRoom
-}
-
-async function currentRoom(){
-    room = await db.currentRoom()
-    return room
-}
-
-
-// db.getRoom()
-// .then()
-// .catch()
-
-// // Stringify JSON before inserting in the database
-// db.addRoom()
-// .then()
-// .catch()
 
 // only get if there is nothing in the database
 // axios
